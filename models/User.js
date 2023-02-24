c102c870fb2e89843f06a2bdfd839f361eb1230e
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-24 17:15:18 +0300

    build the user route

--- models/User.js
+++ models/User.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
 
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);   
 
 const validateUser = (user) => {
     const userSchema = {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {User, validateUser} = require('../models/User');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    const {error} = validateUser(req.body);
+    if(error) res.status(400).send(error.details[0].message);
 
+    let user = await User.findOne({email: req.body.email});
+    if(user) return res.status(400).send('email already in use');
+
+    user = await User.findOne({phoneNumber: req.body.phoneNumber});
+    if(user) return res.status(400).send('phone number already in use');
+
+    user = new User(req.body);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    const token = jwt.sign({
+        _id: user._id, 
+        username: user.username,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        location: user.location  }, "jwtPrivateKey");
+
+    res.send(token);
 });
 
 
