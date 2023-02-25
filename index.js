7af47e1942809243c82e53dab3c82a497e559fd0
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-25 22:21:39 +0300

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const winston = require('winston');
 const app = express();
 
+const cors = require('cors');
+const corsOptions = {
+      origin: 'http://localhost:3000',
+      Credentials: true,
+      optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 require('./startup/config')();
 require('./startup/logging')();
 require('./startup/routes')(app);
