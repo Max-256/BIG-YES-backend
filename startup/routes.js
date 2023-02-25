66837baa0c25bc0d46a8bec08118d8eb995cbfc1
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-25 21:56:30 +0300

    build auth middleware

--- startup/routes.js
+++ startup/routes.js
@@ -1,15 +1,15 @@
 
-const Goods = require('../routes/Goods')
-const User = require('../routes/users')
-const Auth = require('../routes/Auth')
+const goods = require('../routes/Goods')
+const user = require('../routes/users')
+const auth = require('../routes/Auth')
 require('express-async-errors');
 const express = require('express');
 const error = require('../middleware/error');
 
 module.exports = function (app){
     app.use(express.json());
-    app.use('/api/goods', Goods);
-    app.use('/api/user', User);
-    app.use('/api/auth', Auth);
+    app.use('/api/goods', goods);
+    app.use('/api/user', user);
+    app.use('/api/auth', auth);
     app.use(error);
 }
