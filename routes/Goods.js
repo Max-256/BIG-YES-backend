249a4a56a19efc28ce06c139cb373e5937cd1c3d
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-26 10:43:37 +0300

    build post in goods route

--- routes/Goods.js
+++ routes/Goods.js
@@ -4,7 +4,13 @@ const {Good, valiateGood} = require('../models/Good');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    const {error} = valiateGood(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
 
+    const good = new Good(req.body);
+    await good.save();
+
+    res.send(good);
 });
 
 router.put('/:id', async (req, res) => {
